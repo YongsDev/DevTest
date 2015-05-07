1da0c569f60cb0d03cc46db779dd7ebc980ee06e
YongsDev <yongsdev@gmail.com>
2015-05-07 10:52:45 +0900

    fortune.js encapsulate functionality

--- NodeAndExpress/myApp/meadowlark_old2.js
+++ NodeAndExpress/myApp/meadowlark_old2.js
@@ -1,6 +1,14 @@
 var express = require('express');
+
 var app = express();
-var fortune = require('./lib/fortune.js');
+
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
@@ -15,7 +23,8 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 app.get('/about', function(req, res){
-	res.render('about', { fortune: fortune.getFortune()});
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune});
 });
 
 // 404 catch-all handler(middleware)
